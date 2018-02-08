e6173f78464329d39583179cca4d5fef80be5904
Denis Denisenko <ddenisenko1989@gmail.com>
2018-02-08 09:59:48 +0200

    update project after local branch

---
+++ index.php
@@ -0,0 +1,244 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Партнерская страница FIBO Group Academy</title>
+
+    <!-- Jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>-->
+
+    <link rel="stylesheet" href="./fonts/whitney-font.css">
+    <link rel="stylesheet" href="./fonts/font-awesome.css">
+    <link rel="stylesheet" href="./style.css">
+    <script src="./script.js"></script>
+
+</head>
+<body>
+<main>
+    <section class="container-fluid title-block-bg">
+        <div class="container">
+            <h1>Партнерство</h1>
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-1">
+        <div class="container">
+            <h2 class="title-block-1">Партнерская программа от <span class="text-red">Академии <strong>FIBO</strong> Group</span>
+            </h2>
+            <p class="text-block-1">Финансовая компания <strong>FIBO</strong> Group предлагает Вам присоединиться к
+                партнерской программе
+                Академии.</p>
+            <p class="text-block-1">С нашей 2-уровневой партнерской программой Вы сможете зарабатывать каждый день!</p>
+            <p class="text-block-1">Оцените преимущества работы с нами.</p>
+            <div class="centered">
+                <a href="#" class="button-partners"><i class="fa fa-hand-pointer-o"></i>Партнерская программа</a>
+            </div>
+            <img class="big-img-block-1" src="./img/moc-partners-q12.png" alt="FIBO Group">
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-2">
+        <div class="container">
+            <div class="col-xs-12">
+                <div class="row">
+                    <h2>ПОЧЕМУ ИМЕННО МЫ?</h2>
+                    <p class="text-block-2 text-block-2-first-child">Почему мы считаем, что наша партнерская программа
+                        является одной из лучших в нише обучения интернет-трейдингу?</p>
+                    <p class="strong-text-block-2">Потому что Академия FIBO Group предлагает ряд преимуществ.</p>
+                    <p class="text-block-2">Мы очень долго работали не только над нашими обучающими продуктами, но и над
+                        нашей партнерской программой. Анализ партнерских программ на рынке форекс позволил нам выделить
+                        для себя ряд конкурентных преимуществ.</p>
+                </div>
+            </div>
+            <div class="wrapper-advantages advantages-1">
+                <div class="col-xs-12 col-md-4 left-block-advantages">
+                    <div class="row">
+                        <p class="title-advantages">Гибкая система комиссионных выплат</p>
+                        <p class="description-advantages">Процент комиссионных партнера растет параллельно с ростом
+                            числа привлеченных активных клиентов.</p>
+                    </div>
+                </div>
+                <div class="hidden-xs hidden-sm col-md-4 wrapper-pictogram">
+                    <figure>
+                        <img src="./img/icon-partner-6-3-150x150.png" alt="partners programs">
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="row">
+                        <p class="title-advantages">2-уровневая партнерская программа</p>
+                        <p class="description-advantages">Вы можете привлекать как клиентов, так и новых партнеров и
+                            получать доход от обоих категорий привлеченных субъектов</p>
+                    </div>
+                </div>
+            </div>
+            <div class="wrapper-advantages advantages-2">
+                <div class="col-xs-12 col-md-4 left-block-advantages">
+                    <div class="row">
+                        <p class="title-advantages">Обновление рекламных материалов</p>
+                        <p class="description-advantages"><span class="text-red">Академия FIBO Group</span> регулярно
+                            проводит разные рекламные акции. Мы создаем новые материалы под каждую рекламную акцию для
+                            улучшения конверсии партнеров.</p>
+                    </div>
+                </div>
+                <div class="hidden-xs hidden-sm col-md-4 wrapper-pictogram">
+                    <figure>
+                        <img src="./img/icon-partner-6-5-150x150.png" alt="partners programs">
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="row">
+                        <p class="title-advantages">Заказ рекламных материалов</p>
+                        <p class="description-advantages">Если у Вас небольшой опыт работы с партнерскими программами,
+                            Вы можете заказать консультацию у Вашего менеджера по партнерской программе.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 centered">
+                <a href="#" class="button-partners"><i class="fa fa-hand-paper-o"></i>Участвовать в программе</a>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-3">
+        <div class="container">
+            <div class="col-xs-12 description-process-section">
+                <h2>Как Вы сможете заработать?</h2>
+                <p class="subtitle-process-section">Заработать с партнерской программой Академии FIBO Group сможет
+                    каждый!</p>
+            </div>
+            <div class="wrapper-point-block-3">
+                <div class="col-xs-12 col-sm-6 col-md-4 point-block-3 clearfix">
+                    <h4>Вебмастер</h4>
+                    <figure><img src="./img/point-pictogram-1.png" alt="Вебмастер"></figure>
+                    <ul>
+                        <li>Привлекайте целевой трафик.</li>
+                        <li>Размещайте: баннеры, обзорные статьи, рекламные акции.</li>
+                        <li>Отправляйте своим подписчикам рассылки со специальными предложениями от компании.</li>
+                        <li>Варианты писем мы готовы Вам предоставить.</li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 point-block-3 clearfix">
+                    <h4>Аналитик</h4>
+                    <figure><img src="./img/point-pictogram-2.png" alt="Аналитик"></figure>
+                    <ul>
+                        <li>Публикуйте аналитические материалы.</li>
+                        <li>Размещайте партнерские ссылки в своих материалах.</li>
+                        <li>Статьи на тему форекс-обучения.</li>
+                        <li>Дополнительный контент Вы сможете запросить у своего личного менеджера.</li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 point-block-3 clearfix">
+                    <h4>Блогер</h4>
+                    <figure><img src="./img/point-pictogram-3.png" alt="Блогер"></figure>
+                    <ul>
+                        <li>Публикуйте свои статьи на тематических ресурсах с партнерскими материалами Академии FIBO
+                            Group.
+                        </li>
+                        <li>Акции и рекламные предложения помогут Вам привлечь больше клиентов.</li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 point-block-3 col-md-offset-2 clearfix">
+                    <h4>Интернет-предприниматель</h4>
+                    <figure><img src="./img/point-pictogram-4.png" alt="Интернет-предприниматель"></figure>
+                    <ul>
+                        <li>Добавляйте баннеры и другие рекламные материалы Академии FIBO Group на свой сайт.</li>
+                        <li>Расскажите потенциальным клиентам о преимуществах обучения в Академии FIBO Group.</li>
+                        <li>Дополнительную консультацию по размещению партнерских материалов Вы можете получить у своего
+                            личного менеджера.
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-4 point-block-3 col-sm-offset-3 col-md-offset-0 clearfix">
+                    <h4>Продавец сервисов и услуг на рынке форекс</h4>
+                    <figure><img src="./img/point-pictogram-5.png" alt="Продавец сервисов и услуг на рынке форекс">
+                    </figure>
+                    <ul>
+                        <li>В силу того, что Ваша аудитория знакома с рынком форекс, Вам будет достаточно легко привлечь
+                            новых клиентов.
+                        </li>
+                        <li>Можете размещать как материалы для привлечения клиентов, так и для привлечения новых
+                            партнеров.
+                        </li>
+                        <li>Благодаря 2-уровневой программе, у Вас есть возможность зарабатывать за счет обеих групп
+                            аффилиатов.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-4">
+        <div class="container">
+            <h2>Дополнительные возможности партнерской программы<br>от <span class="text-red">Академии FIBO Group</span>
+            </h2>
+            <p class="text-block-4">Продвижение известного бренда всегда проще, чем продвижение малоизвестного или
+                нового.<br> <b>Брокер FIBO Group – один из «китов» рынка форекс.</b></p>
+            <p class="text-block-4">Узнаваемость бренда поможет Вам легко заработать с нашей партнерской программой, а
+                благодаря нашим преимуществам Вы сможете постоянно улучшать конверсию, благодаря чему Ваш доход и %
+                выплат будет постоянно расти.</p>
+            <p class="text-block-4"><b>Зарабатывать с нами – просто и выгодно.</b></p>
+            <p class="text-block-4">Вам необходимо только зарегистрироваться в партнерской программе <span
+                        class="text-red">Академии FIBO Group</span>.<br> Наши менеджеры свяжутся с Вами для уточнения
+                всех деталей.</p>
+        </div>
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-3">
+                    <figure class="support-img">
+                        <img src="./img/an-lebedeva-manager-sup-120x120.jpg" alt="Anna Lebedeva">
+                    </figure>
+                </div>
+                <div class="col-xs-12 col-sm-9 col-lg-6">
+                    <p class="name-manager">Анна Лебедева</p>
+                    <h2 class="position-manager">Ваш персональный менеджер</h2>
+                    <p class="description-manager">По любым вопросам, связанным с обучением или навигацией по сайту, Вы
+                        можете обращаться ко мне, Вашему Персональному менеджеру, Анне Лебедевой.</p>
+                </div>
+                <div class="col-xs-12 col-lg-3 centered">
+                    <a href="mailto:partners@fibo.guru" class="main-button-red"><i class="fa fa-envelope"></i> Email:
+                        partners@fibo.guru</a>
+                    <a href="mailto:manager@fibo.guru" class="main-button-red"><i class="fa fa-envelope"></i> Email:
+                        manager@fibo.guru</a>
+                    <a href="skype:academy_fibo_group?chat" class="main-button-blue"><i class="fa fa-skype"></i> Skype:
+                        academy_fibo_group</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container-fluid partnership-block-6">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-md-4">
+                    <p class="text-block-6">Гибкая система комиссионных выплат</p>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <p class="text-block-6">2-уровневая партнерская программа</p>
+                </div>
+                <div class="col-xs-12 col-md-4">
+                    <div class="col-xs-12 centered">
+                        <div class="row">
+                            <a href="#" class="button-partners"><i class="fa fa-hand-pointer-o"></i> ПРИСОЕДЕНИТЬСЯ К
+                                ПРОГРАММЕ</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+</body>
+</html>
